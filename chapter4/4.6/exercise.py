4d72bcc6e5c6a26ea78090d5c9a3a42949602037
浮生若梦 <microease@163.com>
2019-12-13 10:28:26 +0800

    add

---
+++ chapter4/4.6/exercise.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+# File Name：     exercise
+# Description :
+# Author :       micro
+# Date：          2019/12/13
+SIZE = 7
+array = [[0] * SIZE]
+# 创建一个长度为SIZE*SIZE的二维列表
+for i in range(SIZE - 1):
+    array += [[0] * SIZE]
+
